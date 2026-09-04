e2ffb5cf0c4a0830721225be3ac36ed92d85f331
Shrikant-p95 <patils9505@gmail.com>
2026-09-04 12:28:51 +0530

    diff folder

---
+++ utils/testData.js
@@ -0,0 +1,17 @@
+class testdata {
+    constructor() {
+        this.userName = 'spatil+2@allplan.com';
+        this.Password = '12345678';
+    }
+}
+
+class orderdata {
+    constructor() {
+        this.orderName = 'Test Order';
+    }
+}
+
+export default {
+    testdata: new testdata(),
+    orderdata: new orderdata()
+};
